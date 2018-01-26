bcb173a429dfc7e2c6a93ee847624db89697a3ec
nciilin <670700452@qq.com>
2018-01-26 15:59:32 +0800

    add

--- gulp/src/js/main.js
+++
@@ -1,9 +0,0 @@
- requirejs.config({
-          baseUrl: "src/js",
-          paths: {
-              'jquery': 'lib/bower_components/jquery/jquery.min'
-           }
-      });
-       // 加载入口模块
-      requirejs(['app/index']);
-  
--- webpack/src/js/app/index.js
+++
@@ -1,8 +0,0 @@
-define(['jquery','com/gotop','com/carousel','com/waterfall'], function($,Gotop, Carousel,WaterFall){
-	new Gotop();
-
-	Carousel.init($('.carousel'));
-
-	new WaterFall($('.items'));
-
-});
