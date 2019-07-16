9040f8730c88382a69271e33313807520a2be32a
vincent <670700452@qq.com>
2019-07-16 14:10:09 +0800

    有Bug

--- 轮播/seamlessSlide/main.js
+++ 轮播/seamlessSlide/main.js
@@ -1,14 +1,31 @@
 
 let n
 init()
-setInterval(()=>{
+let timer = setInterval(()=>{
   makeLeave(getImage(n))
     .one('transitionend', (e)=>{
       makeEnter($(e.currentTarget))
     })
   makeCurrent(getImage(n+1))
   n += 1
-},3000)
+},2000)
+
+document.addEventListener('visibilitychange', function(e) {
+  if(document.hidden) {
+    window.clearInterval(timer)
+  } else {
+    timer = setInterval(()=>{
+      makeLeave(getImage(n))
+        .one('transitionend', (e)=>{
+          makeEnter($(e.currentTarget))
+        })
+      makeCurrent(getImage(n+1))
+      n += 1
+    },2000)
+  }
+})
+
+
 
 function getImage(n){
   return $(`.images > img:nth-child(${x(n)})`)
